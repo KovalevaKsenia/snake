a6b8277dd3e0d5e5ccbe6214377047cfe8899cd7
Ksenia Kovaleva <ksenia.shinkorenko@gmail.com>
2019-11-17 23:51:41 +0300

    Фигура. Наследование

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Console.SetWindowSize(80, 25);
 
-            p1.Draw();
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            upline.Draw();
+            downline.Draw();
+            leftline.Draw();
+            rightline.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Draw();
-
-            VerticalLine line1 = new VerticalLine(8, 13, 5, '+');
-            line1.Draw();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
         }
